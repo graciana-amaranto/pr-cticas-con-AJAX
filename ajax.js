1a63c9642c515aec213b5a429feed1187358e60f
whitenoise333 <graciana_amaranto@hotmail.com>
2022-08-16 18:26:29 -0300

    Fetch usando promesas

--- ajax.js
+++ ajax.js
@@ -1,7 +1,7 @@
 //funcion anonima autoejecutable (()=>{})();, forma de encapsulamiento sin necesidad de recurrir a los módulos.
 
 //Objeto XMLHttpRequest
-//Quiero agregar una lista ordenada dinámicamente al html usando ajax:
+//Quiero agregar una lista ordenada de usuarios dinámicamente al html usando ajax:
 (() =>{
     const xhr = new XMLHttpRequest(),    
         $xhr = document.getElementById("xhr"),
@@ -28,7 +28,7 @@
         }
     });  //este evento se activa cuando detecta algun cambio de estado (aborto, error, completado ))       
 
-    xhr.open("GET", "https://jsonplaceholder.typicode.com/user" )
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users" )
 
     xhr.send();
 
@@ -39,4 +39,33 @@
     3) Abrir la peticiom, estalecer el recurso o método (GET, POST) y la url a la que quiero acceder-
     4) Enviar la peticion con send().
     */ 
+})();
+
+//Objeto API Fetch
+(() =>{
+    const $fetch = document.getElementById("fetch"),
+        $fragment = document.createDocumentFragment();
+    
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) =>{return res.ok ? res.json() : Promise.reject(res);})  //si res.ok es true, entonces que convierta. si no, reject me manda directamente al catch.
+        //res.json() me trae lo que hay en la pag y lo convierte en objeto (como parse)
+    
+    .then((json) =>{
+        json.forEach(el => { //por cada elemento que tenga el objeto con la info, creame un li con el nombre, email y tel.
+            const $li = document.createElement("li");
+            $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone} `;
+            $fragment.appendChild($li);  
+        });
+
+        $fetch.appendChild($fragment);
+    })
+    .catch((err) =>{
+        let message = err.statusText || "Ocurrio un error";  //a veces statusText viene vacio
+        $fetch.innerHTML = `Error ${err.status}: ${message}  `
+    })
+    .finally(); //el finally es opcional
+
+    /*
+    El primer then valida si existe la pagina, y es true, convierte el json a objeto (podria ser json a texto)
+    El segundo then hace toda la logica*/
 })();
